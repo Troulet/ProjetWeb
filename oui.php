908cef1a38385823b21ff56295ccb8e2988d3d8c
Troulet <73887720+Troulet@users.noreply.github.com>
2021-03-22 17:00:28 +0100

    Add files via upload

--- oui.php
+++ oui.php
@@ -2,8 +2,8 @@
 $bdd = new PDO('mysql:host=localhost;dbname=projetweb', 'root', 'Tri27rou', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 $stmt = $bdd->prepare("SELECT mail, Mot_de_passe FROM utilisateur WHERE mail = :mail AND Mot_de_passe = :mdp;");
-$mail = $_POST["login"];
-$mdp = $_POST["mdp"];
+$mail = htmlspecialchars($_POST["Login"]);
+$mdp = htmlspecialchars($_POST["Mdp"]);
 
 $stmt->bindParam(':mail', $mail, PDO::PARAM_STR);
 $stmt->bindParam(':mdp', $mdp, PDO::PARAM_STR);
@@ -24,3 +24,12 @@ else{
     }
 }
 ?>
+
+<html>
+    <head>
+    </head>
+    <body>
+    </body>
+    <footer>
+    </footer>
+</html>
