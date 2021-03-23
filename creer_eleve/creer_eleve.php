e3ddcc0442447ea3b00c2681cd7b8c9da0f1e8d2
Troulet <73887720+Troulet@users.noreply.github.com>
2021-03-23 14:05:08 +0100

    Add files via upload

---
+++ creer_eleve/creer_eleve.php
@@ -0,0 +1,34 @@
+<?php
+$stmt = $bdd->prepare("INSERT INTO localite(Localite) VALUES (:localite) AND utilisateur(Mail, Mot_de_passe, Nom_User, Prenom_User, id_Localite) VALUES (:mail, :mdp, :nom, :prenom, (SELECT id FROM localite WHERE Localite = :localite)) AND eleve(id_Utilisateur, Promotion, Delegue, Droits_Delegue, Fiche_Validation, Mail, Mot_de_passe, Nom_User, Prenom_user, id_Utilisateur_Pilote, id_Pilote, id_Localite) VALUES ((SELECT id FROM utilisateur WHERE Mot_de_passe = :mdp AND Mail = :mail AND Nom_user = :nom AND Prenom_user = :prenom), :promotion, :delegue, :droits, :fiche, (SELECT Mail FROM utilisateur WHERE Mail = :mail), (SELECT Mot_de_passe FROM utilisateur WHERE Mot_de_passe = :mdp), (SELECT Nom_User FROM utilisateur WHERE Nom_user = :nom), (SELECT Prenom_User FROM utilisateur WHERE Prenom_user = :prenom), (SELECT id_Utilisateur FROM pilote WHERE Promotion = :promotion), (SELECT id FROM pilote WHERE Promotion = :promotion), (SELECT id FROM Localite WHERE Localite = :localite))")
+
+$localite = htmlspecialchars($_POST["Localite"]);
+$mail = htmlspecialchars($_POST["Mail"]);
+$mdp = htmlspecialchars($_POST["Mdp"]);
+$nom = htmlspecialchars($_POST["Nom"]);
+$prenom = htmlspecialchars($_POST["Prenom"]);
+$promotion = htmlspecialchars($_POST["Promotion"]);
+$delegue = htmlspecialchars($_POST["Delegue"]);
+$droits = htmlspecialchars($_POST["Droits"]);
+$fiche = htmlspecialchars($_POST["Fiche"]);
+
+$data[
+    'localite' => $localite, //string
+    'mail' => $mail, //string
+    'mdp' => $mdp, //string
+    'nom' => $nom, //string
+    'prenom' => $prenom, //string
+    'promotion' => $promotion, //string
+    'delegue' => $delegue, //bool
+    'droits' => $droits, //string
+    'fiche' => $fiche, //fichier .pdf ici
+];
+
+if(!$stmt->execute($data)){
+    echo "erreur<br/>";
+}
+else{
+    echo "Insertion effectué !<br/>";
+}
+
+$stmt->closecursor();
+?>
