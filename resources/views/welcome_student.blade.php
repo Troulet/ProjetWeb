26244f31bbb41cee83afa2b029a96441e2983fa3
Clémentine COUTANT <clementine.coutant@viacesi.fr>
2021-03-28 22:56:49 +0200

    kiko

--- resources/views/welcome_student.blade.php
+++ resources/views/welcome_student.blade.php
@@ -4,11 +4,9 @@
     <meta charsert="utf-8" />
     <link rel="stylesheet" href="welcome_student.css" />
     <title>LES P'TITES ANNONCES</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/vendors/fontawesome/css/all.min.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css2?family=Happy+Monkey&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <?php 
+          include("head_global.blade.php"); 
+    ?>
 </head>
 <body>
     <div id="1">
