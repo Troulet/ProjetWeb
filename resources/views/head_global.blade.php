8a00f948c35e5296d89720274dd925b22becb52a
Clémentine COUTANT <clementine.coutant@viacesi.fr>
2021-03-29 00:51:19 +0200

    monster 4ever

--- resources/views/head_global.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-        <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="./fontawesome/css/all.min.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link href="https://fonts.googleapis.com/css2?family=Happy+Monkey&display=swap" rel="stylesheet">
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-</html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,8 +4,15 @@
         <meta charsert="utf-8" />
         <link rel= "stylesheet" href="connexion.css"/>
         <title>LES P'TITES ANNONCES</title>
+        <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="./fontawesome/css/all.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="https://fonts.googleapis.com/css2?family=Happy+Monkey&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
     <body>
+
         <div id="1">
             <img class="logo2" src="img/logo_cesi2.0.png">
             <h1>Authentification</h1>
