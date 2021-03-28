10b59873b598f646fce707266811b2bad0a51127
Clémentine COUTANT <clementine.coutant@viacesi.fr>
2021-03-28 19:36:08 +0200

    oui

---
+++ resources/views/welcome_pilot.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charsert="utf-8" />
+        <link rel= "stylesheet" href="connexion.css"/>
+        <title>LES P'TITES ANNONCES</title>
+        <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="./fontawesome/css/all.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="https://fonts.googleapis.com/css2?family=Happy+Monkey&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    </head>
+    <body>
+        <div id="1">
+        <img class="logo2" src="../img/logo_cesi2.0.png">
+        <h1>LES P'TITES ANNONCES.</h1>
+        <h2>Cherche et trouve le stage qui te correspond !</h2>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand">Menu</a>
+                <!--<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>-->
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Offre</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Elève</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Aide</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+        <div class="container">
+            <div class="carousel slide" data-ride="carousel" id="multi_item" data-intervale="2000">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="row">
+                            <div class="col-sm">
+                                <img class="d-block w-100" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="1 slide">
+                                <div class="card-body">
+                                    <h5 class="card-title">Elève</h5>
+                                    <p class="card-text">
+                                        Statistique de cet élève
+                                    </p>
+                                    <a href="#" class="btn btn-primary">Aller voir</a>
+                                </div>
+                            </div>
+                            <div class="col-sm">
+                                <img class="d-block w-100" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="2 slide">
+                                <div class="card-body">
+                                    <h5 class="card-title">Elève</h5>
+                                    <p class="card-text">
+                                        Statistique de cet élève
+                                    </p>
+                                    <a href="#" class="btn btn-primary">Aller voir</a>
+                                </div>
+                            </div>
+                            <div class="col-sm"><img class="d-block w-100" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="3 slide"></div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row">
+                            <div class="col-sm"><img class="d-block w-100" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="4 slide"></div>
+                            <div class="col-sm"><img class="d-block w-100" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="5 slide"></div>
+                            <div class="col-sm"><img class="d-block w-100" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="6 slide"></div>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#multi_item" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#multi_item" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+        <footer>
+            <img class="logo" src="C:\xampp\htdocs\Projet\img\logo_cesi.png">
+            <ul>
+                <li>
+                    <a href="https://ent.cesi.fr/?RH=&ksession=9e43032b-00b8-446a-8782-5768fbf6406f" title="ENT" target="blank">ENT</a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/school/cesiecoledingenieurs/?trk=public_profile_topcard-school" title="Linkedin" target="blank">Linkedin</a>
+                </li>
+            </ul>
+            <button class="bouton" type="button">Support</button>
+            <script src="../assets/vendors/jquery/jquery-3.5.1.min.js"></script>
+            <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+            <script src="../assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+        </footer>
+    </body>
+</html>
