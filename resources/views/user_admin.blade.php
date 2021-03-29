b12e89b530d12f10ebc84107b23200c1e1f7c9b3
Clémentine COUTANT <clementine.coutant@viacesi.fr>
2021-03-29 05:19:49 +0200

    done

--- resources/views/user_admin.blade.php
+++ resources/views/user_admin.blade.php
@@ -2,8 +2,8 @@
 <html>
     <head>
         <meta charsert="utf-8" />
-        <link rel= "stylesheet" href="connexion.css"/>
-        <title> Stage - Authentification</title>
+        <link rel= "stylesheet" href="user_admin.css"/>
+        <title>LES P'TITES ANNONCES</title>
         <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="./fontawesome/css/all.min.css">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -15,8 +15,89 @@
         <?php 
           include("header.blade.php"); 
         ?>
-
-
+            <div class="filtre">
+                <h3>Filtrer les résultats</h3>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+                <div>
+                    <h3>Catégories</h3>
+                        <div>
+                            <input type="checkbox" id="student" name="student">
+                            <label for="student">Étudiant</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="representative" name="representative">
+                            <label for="representative">Délégué</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="pilot" name="pilot">
+                            <label for="pilot">Pilote</label>
+                        </div>
+                    <h3>Localité</h3>
+                        <div>
+                            <input type="checkbox" id="paris" name="paris">
+                            <label for="paris">Paris</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="bordeaux" name="bordeaux">
+                            <label for="bordeaux">Bordeaux</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="merignac" name="merignac">
+                            <label for="merignac">Mérignac</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="lyon" name="lyon">
+                            <label for="lyon">Lyon</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="olympe" name="olympe">
+                            <label for="olympe">Mont Olympe</label> 
+                        </div>
+                    <h3>Promotion</h3>
+                        <div>
+                            <input type="checkbox" id="info" name="info">
+                            <label for="info">A2 Informatique</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="generaliste" name="generaliste">
+                            <label for="generaliste">A2 Généraliste</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="btp" name="btp">
+                            <label for="btp">A2 BTP</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="s3e" name="s3e">
+                            <label for="s3e">A2 S3E</label>
+                        </div>
+                </div>
+            </div>
+            <div class="profil_card">
+                <h2>Profil Utilisateur</h2>
+                <div class="card">
+                    <div class="card-body">
+                        This is some text within a card body.
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        This is some text within a card body.
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        This is some text within a card body.
+                    </div>
+                </div>
+            </div>
+        <div>
+            <input class="creation_E"
+           type="button"
+           value="CRÉER UN PROFIL UTILISATEUR">
+        </div>
     </body>
 
     <?php 
