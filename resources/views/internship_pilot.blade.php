0ebc35e1be69c4b2fe026e205f50c71d38389355
Kitawiim <49398010+Kitawiim@users.noreply.github.com>
2021-03-30 22:13:30 +0200

    Update internship_pilot.blade.php

--- resources/views/internship_pilot.blade.php
+++ resources/views/internship_pilot.blade.php
@@ -2,8 +2,8 @@
 <html>
     <head>
         <meta charsert="utf-8" />
-        <link rel= "stylesheet" href="internship_pilot.css"/>
-        <link rel= "stylesheet" href="header_footer.css"/>
+        <link rel= "stylesheet" href="../resources/views/internship_pilot.css"/>
+        <link rel= "stylesheet" href="../resources/views/header_footer.css"/>
         <title>LES P'TITES ANNONCES</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link href="https://fonts.googleapis.com/css2?family=Happy+Monkey&display=swap" rel="stylesheet">
@@ -13,7 +13,7 @@
     </head>
     <body>
         <?php 
-          include("header.blade.php"); 
+          include("../resources/views/header.blade.php"); 
         ?>
     </br>
             <div class="filtre">
@@ -190,6 +190,6 @@ data-mdb-ride="carousel"
     </section>
     </body>
     <?php 
-          include("footer.blade.php"); 
+          include("../resources/views/footer.blade.php"); 
     ?>
 </html>
