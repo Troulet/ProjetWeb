50e80dfb00096ebb536dfd0447e6285dcf9ca666
Clémentine COUTANT <clementine.coutant@viacesi.fr>
2021-03-30 18:29:35 +0200

    ich ich

---
+++ resources/views/contact_student.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charsert="utf-8" />
+        <link rel= "stylesheet" href="postulate.css"/>
+        <title>LES P'TITES ANNONCES</title>
+        <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="./fontawesome/css/all.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="https://fonts.googleapis.com/css2?family=Happy+Monkey&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head>
+        
+    <body>
+        <?php 
+          include("header.blade.php"); 
+        ?>
+
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Contact administrateur du centre</h5>
+                <p class="card-text">
+                Mail : 
+                </p>
+                <button type="button" class="btn btn-primary">Contacter</button>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Contact pilotes</h5>
+                <p class="card-text">
+                Mail pilote 1 :
+                </p>
+                <button type="button" class="btn btn-primary">Contacter</button>
+                <p class="card-text">
+                Mail pilote 2 :
+                </p>
+                <button type="button" class="btn btn-primary">Contacter</button>
+                <p class="card-text">
+                Mail pilote 3 :
+                </p>
+                <button type="button" class="btn btn-primary">Contacter</button>
+            </div>
+        </div>
+
+    </body>
+    <?php 
+        include("footer.blade.php"); 
+    ?>
