264b8a98d7cde23a4ab59fec481151cf6ad46333
Kitawiim <49398010+Kitawiim@users.noreply.github.com>
2021-03-30 22:04:17 +0200

    Update contact_pilot.blade.php

--- resources/views/contact_pilot.blade.php
+++ resources/views/contact_pilot.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charsert="utf-8" />
-        <link rel= "stylesheet" href="header_footer.css"/>
+        <link rel= "stylesheet" href="../resources/views/header_footer.css"/>
         <title>LES P'TITES ANNONCES</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link href="https://fonts.googleapis.com/css2?family=Happy+Monkey&display=swap" rel="stylesheet">
@@ -12,7 +12,7 @@
         
     <body>
         <?php 
-          include("header.blade.php"); 
+          include("../resources/views/header.blade.php"); 
         ?>
 
         <div class="card">
@@ -67,5 +67,5 @@
         </div>
     </body>
     <?php 
-        include("footer.blade.php"); 
+        include("../resources/views/footer.blade.php"); 
     ?>
