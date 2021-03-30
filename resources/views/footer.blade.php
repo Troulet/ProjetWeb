11f14b94342feac3fef39cc13072654216abacd6
Kitawiim <49398010+Kitawiim@users.noreply.github.com>
2021-03-30 22:09:25 +0200

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<link rel= "stylesheet" href="../resources/views/header_footer.css"/>
 <footer>
-    <img class="logo" src="../img/logo_cesi.png">
+    <img class="logo" src="../resources/img/logo_cesi.png">
     <ul>
         <li>
             <a href="https://ent.cesi.fr/?RH=&ksession=9e43032b-00b8-446a-8782-5768fbf6406f" title="ENT" target="blank">ENT</a>
