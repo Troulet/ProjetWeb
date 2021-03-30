26b92e6d0e0584adfbd243c7aac9d640a0bee18e
Kitawiim <49398010+Kitawiim@users.noreply.github.com>
2021-03-30 23:17:53 +0200

    Update header.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
 <link rel= "stylesheet" href="../resources/views/header_footer.css"/>
-<body>
+
+<body onload="print_page()">
     <div id="1">
        <div id="header">
         <div class="titre">
@@ -9,86 +10,108 @@
             <h1>LES P'TITES ANNONCES</h1>
             <h2>Cherche et trouve le stage qui te correspond !</h2>
         </div>
-            if(ID==ID_student) {
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand">Menu</a>
-                    <!--<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                      <span class="navbar-toggler-icon"></span>
-                    </button>-->
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Offre</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Contact</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Candidature</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Aide</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-} else if(ID==ID_Pilote){
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand">Menu</a>
-                <!--<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>-->
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Offre</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Elève</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Aide</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-} else {
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-               <div class="container-fluid">
-                        <a class="navbar-brand">Menu</a>
-                        <!--<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>-->
-                        <div class="collapse navbar-collapse" id="navbarNav">
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="#">Offre</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Elève</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Contact</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Utilisateur</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Aide</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-}
         </div>
     </div>
     
+    <div id="page_loaded"></div>
+    
+    <script type="javascript">
+    
+        function print_page() {
+            var html='';
+            
+            if($user_type==0)  {
+            
+                html= '<div id="student">
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                        <div class="container-fluid">
+                            <a class="navbar-brand">Menu</a>
+                            <!--<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                              <span class="navbar-toggler-icon"></span>
+                            </button>-->
+                            <div class="collapse navbar-collapse" id="navbarNav">
+                                <ul class="navbar-nav">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" aria-current="page" href="#">Offre</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Contact</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Candidature</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Aide</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>';
+            }
+            
+            else if($user_state==1) {
+            
+                html='<div id="pilot">
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                            <div class="container-fluid">
+                                <a class="navbar-brand">Menu</a>
+                                <!--<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                          <span class="navbar-toggler-icon"></span>
+                        </button>-->
+                                <div class="collapse navbar-collapse" id="navbarNav">
+                                    <ul class="navbar-nav">
+                                        <li class="nav-item">
+                                            <a class="nav-link active" aria-current="page" href="#">Offre</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Contact</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Elève</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Aide</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>';
+            }
+            else {
+            
+                html='<div id="admin">
+                        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                               <div class="container-fluid">
+                                        <a class="navbar-brand">Menu</a>
+                                        <!--<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                  <span class="navbar-toggler-icon"></span>
+                                </button>-->
+                                        <div class="collapse navbar-collapse" id="navbarNav">
+                                            <ul class="navbar-nav">
+                                                <li class="nav-item">
+                                                    <a class="nav-link active" aria-current="page" href="#">Offre</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="#">Elève</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="#">Contact</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="#">Utilisateur</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="#">Aide</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </nav>
+                            </div>';
+                }
+            document.getElementById("page_loaded").innerHTML = html;
+        }
+    </script>
 </body>
