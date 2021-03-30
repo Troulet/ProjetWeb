32175afd278008b1d0790f9962328bc025066281
Kitawiim <49398010+Kitawiim@users.noreply.github.com>
2021-03-30 22:14:54 +0200

    Update internship_student.blade.php

--- resources/views/internship_student.blade.php
+++ resources/views/internship_student.blade.php
@@ -2,8 +2,8 @@
 <html>
     <head>
         <meta charsert="utf-8" />
-        <link rel= "stylesheet" href="internship_student.css"/>
-        <link rel= "stylesheet" href="header_footer.css"/>
+        <link rel= "stylesheet" href="../resources/views/internship_student.css"/>
+        <link rel= "stylesheet" href="../resources/views/header_footer.css"/>
         <title>LES P'TITES ANNONCES</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link href="https://fonts.googleapis.com/css2?family=Happy+Monkey&display=swap" rel="stylesheet">
@@ -13,7 +13,7 @@
     </head>
     <body>
         <?php 
-        include("header.blade.php"); 
+        include("../resources/views/header.blade.php"); 
         ?>
     </br>
         <div class="filtre">
@@ -259,6 +259,6 @@
 
 </body>
     <?php 
-    include("footer.blade.php"); 
+    include("../resources/views/footer.blade.php"); 
     ?>
 </html>
