8c369d892923127c726da862db754df8302a78a7
Kitawiim <49398010+Kitawiim@users.noreply.github.com>
2021-03-30 23:19:12 +0200

    Update header.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -114,4 +114,5 @@
             document.getElementById("page_loaded").innerHTML = html;
         }
     </script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 </body>
