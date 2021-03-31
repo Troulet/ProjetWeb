4ef37a0a7018642e156febc3f3a7323936a0c3ae
Kitawiim <49398010+Kitawiim@users.noreply.github.com>
2021-03-31 13:23:19 +0200

    Update create_modify_pilot.blade.php

--- resources/views/create_modify_pilot.blade.php
+++ resources/views/create_modify_pilot.blade.php
@@ -12,7 +12,7 @@
         </div>
     </div>
     <div>
-        {!! Form::open(['url' => '/Offer', 'method' => 'post']) !!}
+        {!! Form::open(['url' => '/admin/pilot', 'method' => 'post']) !!}
         <fieldset>
             <legend>Créer un pilote</legend>
      
