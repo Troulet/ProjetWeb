0225a36b57bc93994fc579c1ff58df60b4f072af
Kitawiim <49398010+Kitawiim@users.noreply.github.com>
2021-03-31 14:38:12 +0200

    Add files via upload

---
+++ resources/views/inform_step_4.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<link rel= "stylesheet" href="../resources/views/header_footer.css"/>
+<body>
+    <div id="1">
+       <div id="header">
+        <div class="titre">
+            <img class="logo2" src="../resources/img/logo_cesi2.0.png">
+            <h1>LES P'TITES ANNONCES</h1>
+            <h2>Cherche et trouve le stage qui te correspond !</h2>
+        </div>
+        </div>
+    </div>
+    <div>
+        {!! Form::open(['url' => '/Stats', 'method' => 'post']) !!}
+        <fieldset>
+            <legend>Information étape 4 </legend>
+     
+            <label for="Validation_Sheet">Fiche de validation : </label>
+            <input type="text" name="Validation_Sheet" id="Validation_Sheet" />
+     
+
+        </fieldset>
+        <button type="submit" class="btn">ENVOYER</button>
+        
+        {!! Form::close() !!}
+    </div>
+    </script>
+</body>
