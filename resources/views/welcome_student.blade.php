b329bc170283415a2738a800b634d74db7bdfc61
Kitawiim <49398010+Kitawiim@users.noreply.github.com>
2021-03-31 02:14:45 +0200

    Update welcome_student.blade.php

--- resources/views/welcome_student.blade.php
+++ resources/views/welcome_student.blade.php
@@ -20,6 +20,7 @@
             <div class="carousel-inner">
                 <div class="carousel-item active">
                     <div class="row">
+                    
                         <div id="banner-one" class="col-sm">
                             <img class="d-block w-100 desk-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="1 slide">
                             <img class=" w-100  mobile-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="1 slide">
@@ -31,6 +32,7 @@
                                 <a href="#" class="btn btn-primary">Aller voir</a>
                             </div>
                         </div>
+                        
                         <div id="banner-two" class="col-sm">
                             <img class="d-block w-100 desk-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="2 slide">
                             <img class=" w-100  mobile-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="2 slide">
@@ -42,7 +44,18 @@
                                 <a href="#" class="btn btn-primary">Aller voir</a>
                             </div>
                         </div>
-                        <div class="col-sm"><img class="d-block w-100" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="3 slide"></div>
+                        
+                        <div class="col-sm">
+                            <img class="d-block w-100 mobile-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="3 slide">
+                            <img class="d-block w-100 desk-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="3 slide">
+                            <div class="card-body">
+                                 <h5 class="card-title">Stage</h5>
+                                 <p class="card-text">
+                                     Thales recrute un stagiaire en informatique pour du développement.
+                                 </p>
+                                 <a href="#" class="btn btn-primary">Aller voir</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="carousel-item">
@@ -53,17 +66,17 @@
                     </div>
                 </div>
             </div>
-            <a class="carousel-control-prev" href="#multi_item" role="button" data-slide="prev">
+            <a id="go_previous" class="carousel-control-prev" href="#multi_item" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
             </a>
-            <a class="carousel-control-next" href="#multi_item" role="button" data-slide="next">
+            <a id="go_next" class="carousel-control-next" href="#multi_item" role="button" data-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
             </a>
         </div>
     </div>
-    <div class="wishlist">
+    <div id="wishlist_student" class="wishlist">
         <div class="list-group">
             <h3>Wish-List</h3>
             <a href="#" class="list-group-item list-group-item-action">Choix 1</a>
