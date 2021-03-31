851a3f832d2b8c299074a263a6fce24d93acc937
Kitawiim <49398010+Kitawiim@users.noreply.github.com>
2021-03-31 02:23:13 +0200

    Update student_pilot.blade.php

--- resources/views/student_pilot.blade.php
+++ resources/views/student_pilot.blade.php
@@ -10,16 +10,19 @@
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
+    
     <body>
         <?php 
           include("../resources/views/header.blade.php"); 
         ?>
             <div class="filtre">
+            
                 <h3>Filtrer les résultats</h3>
                 <form class="d-flex">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-outline-success" type="submit">Search</button>
                 </form>
+                
                 <div class="titres">
                     <h3>Catégories</h3>
                         <div>
@@ -30,12 +33,14 @@
                             <input type="checkbox" id="representative" name="representative">
                             <label for="representative">Délégué</label>
                         </div>
+                        
                     <h3>Promotion</h3>
                         <div>
                             <input type="checkbox" id="info" name="info">
                             <label for="info">A1 Informatique</label>
                         </div>
                         <!--insérer boucle qui affiche les promo assigné au pilote -->
+                        
                     <h3>Avancement dans la recherche de stage</h3>
                         <div>
                             <input type="checkbox" id="no_intership" name="no_intership">
@@ -51,7 +56,9 @@
                         </div>
                 </div>
             </div>
+            
             <div class="profil_card">
+            
                 <h2>Profil Élève</h2>
                 <div class="card">
                     <div class="card-body">
@@ -62,7 +69,9 @@
                         <button type="button" class="btn">MODIFIER</button>
                     </div>
                 </div>
+                
                 <br/>
+                
                 <div class="card">
                     <div class="card-body">
                        Données de l'élève (login, promo, centre, nbre de candidature, email etc)
@@ -72,7 +81,9 @@
                         <button type="button" class="btn">MODIFIER</button>
                     </div>
                 </div>
+                
                 <br/>
+                
                 <div class="card">
                     <div class="card-body">
                        Données de l'élève (login, promo, centre, nbre de candidature, email etc)
@@ -83,6 +94,7 @@
                     </div>
                 </div>
             </div>
+            
         <div class="create_user">
             <button type="button" class="btn">CRÉER UN PROFIL UTILISATEUR</button>
         </div>
