8162ef54cd045e8d9cfefe9de2fa7cd7e0ed706c
Inès Casenave <ines.ath.casenave@gmail.com>
2021-04-03 12:31:34 +0200

    pb bootstrap footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -15,5 +15,6 @@
     </ul>
     </div>
     <button class="bouton" type="button">Support</button>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </footer>
 
