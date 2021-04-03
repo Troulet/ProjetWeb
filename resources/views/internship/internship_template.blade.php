3e8e1d1fcce08048786e04514b7080271f39f08a
Inès Casenave <ines.ath.casenave@gmail.com>
2021-04-03 14:30:19 +0200

    pour toi clem

--- resources/views/internship/internship_template.blade.php
+++ resources/views/internship/internship_template.blade.php
@@ -2,24 +2,29 @@
 
 @section('carousel_offer')
             @foreach($dataOffer as $Offer)
+                $i++
+                <img class="d-block w-100 desk-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt=""$i." slide">
+                <img class="w-100  mobile-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt=""$i." slide">
                 <h5 class="card-title">Stage chez {{$Offer['Enterprise_Name']}}</h5>
                 <p class="card-text">
-                {{$Offer['Description']}} </br>
-                Compétences recherchées : {{$Offer['Skills_Researched']}} </br>
-                Promotion ciblée : {{$Offer['Promotion_Researched']}} </br>
-                Nombre de places : {{$Offer['Number_Of_Places']}} </br>
-                Localisation : {{$Offer['Localisation_Name']}} </br>
+                    {{$Offer['Description']}} </br>
+                    Compétences recherchées : {{$Offer['Skills_Researched']}} </br>
+                    Promotion ciblée : {{$Offer['Promotion_Researched']}} </br>
+                    Nombre de places : {{$Offer['Number_Of_Places']}} </br>
+                    Localisation : {{$Offer['Localisation_Name']}} </br>
                 </p>
                 <a href="#" class="btn btn-primary">Aller voir</a>
             @endforeach
-@section('carousel_entreprise')
             @foreach($dataEnterprise as $Enterprise)
+                $i++
+                <img class="d-block w-100 desk-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt=""$i." slide">
+                <img class="w-100  mobile-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt=""$i." slide">
                 <h5 class="card-title">Entreprise {{$Enterprise['Enterprise_Name']}}</h5>
                 <p class="card-text">
-                Travaille dans le secteur : {{$Enterprise['Activity_Sector']}} </br>
-                A déjà pris {{$Enterprise['Cesi_Student_Taken']}} élèves. </br>
-                Nombre de places : {{$Offer['Number_Of_Places']}} </br>
-                Se situe à {{$Enterprise['Localisation_Name']}} ! </br>
+                    Travaille dans le secteur : {{$Enterprise['Activity_Sector']}} </br>
+                    A déjà pris {{$Enterprise['Cesi_Student_Taken']}} élèves. </br>
+                    Nombre de places : {{$Offer['Number_Of_Places']}} </br>
+                    Se situe à {{$Enterprise['Localisation_Name']}} ! </br>
                 </p>
                 <a href="#" class="btn btn-primary">Aller voir</a>
             @endforeach
