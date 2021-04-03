4cf33d08f5db2ab1e0394a78d0c69d0ef4c19c28
Inès Casenave <ines.ath.casenave@gmail.com>
2021-04-03 12:23:33 +0200

    pouet

--- resources/views/internship/internship_student.blade.php
+++ resources/views/internship/internship_student.blade.php
@@ -105,23 +105,36 @@
                   <div class="row">
                       <div id="banner-one" class="col-sm">
                           <img class="d-block w-100 desk-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="1 slide">
-                          <img class=" w-100  mobile-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="1 slide">
+                          <img class="w-100  mobile-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="1 slide">
                           <div class="card-body">
+                              @yield('carousel_enterprise')
+                          </div>
+                      </div>
+                      <div id="banner-two" class="col-sm">
+                          <img class="d-block w-100 desk-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="2 slide">
+                          <img class="w-100  mobile-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="2 slide">
+                          <div class="card-body">
+                              @yield('carousel_enterprise')
+                          </div>
+                      </div>
+                      <div id="banner-three" class="col-sm">
+                         <img class="d-block w-100 desk-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="3 slide">
+                         <img class="w-100  mobile-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="3 slide">
+                         <div class="card-body">
                               @yield('carousel_enterprise')
                           </div>
                       </div>
                   </div>
               </div>
-          </div>
-      </div>
-  </div>
               <div class="carousel-item">
                   <div class="row">
                       <div class="col-sm">
-                      <img class="d-block w-100" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="4 slide">
-                      <img class=" w-100  mobile-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="4 slide">
-                          <div class="card-body">
-                              @yield('carousel_enterprise')
+                          <div id="banner-four" class="col-sm">
+                             <img class="d-block w-100 desk-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="4 slide">
+                             <img class="w-100  mobile-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="4 slide">
+                             <div class="card-body">
+                                  @yield('carousel_enterprise')
+                              </div>
                           </div>
                       </div>
                       <div class="col-sm"><img class="d-block w-100" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="5 slide"></div>
