95db3aea0fa4884050f56ca4f5a4553ac9207cda
Clémentine COUTANT <clementine.coutant@viacesi.fr>
2021-04-03 18:13:33 +0200

    io

--- resources/views/User_profil_template.blade.php
+++ resources/views/User_profil_template.blade.php
@@ -22,4 +22,28 @@
     </div>
 @endsection
 
-@section('')
+@section('pilot_profil')
+<div class="card-body">
+            <p>
+                Pilote :
+                Id : {{$user['Users_id']}}
+                Email : {{$user['email']}}
+                Mot de passe : {{$user['password']}}
+                Prénom : {{$user['First_Name']}}
+                Nom : {{$user['Last_name']}}
+                Promotion : {{$user['Promotion']}}
+                Localité : {{$user['Localisation_Name']}}
+            </p>
+    </div>
+@endsection
+
+@section
+<div class="card-body">
+    <p>
+        Information Admin :
+        Email : {{$user['email']}}
+        Prénom : {{$user['First_Name']}}
+        Nom : {{$user['Last_name']}}
+    </p>
+</div>
+@endsection
