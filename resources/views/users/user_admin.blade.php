5737ce6a5bbf62cd03773e3b896cae49826821c1
Inès Casenave <ines.ath.casenave@gmail.com>
2021-04-04 19:04:33 +0200

    user admin avec search

--- resources/views/users/user_admin.blade.php
+++ resources/views/users/user_admin.blade.php
@@ -12,68 +12,68 @@
     </head>
     <body>
         @include('header')
-            <!--<div class="filtre">
-                <h3>Filtrer les résultats</h3>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-                <div>
-                    <h3>Catégories</h3>
-                        <div>
-                            <input type="checkbox" id="student" name="student">
-                            <label for="student">Étudiant</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" id="representative" name="representative">
-                            <label for="representative">Délégué</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" id="pilot" name="pilot">
-                            <label for="pilot">Pilote</label>
-                        </div>
+            <div class="filtre">
+              <h3>Filtrer les résultats</h3>
+                {!! Form::open(['url' => '/User_Search', 'method' => 'post']) !!}
+                <input class="form-control me-2" type="search" name="research" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+                {!! Form::close() !!}
+                <!--<div>
+                     <h3>Catégories</h3>
+                            <div>
+                                <input type="checkbox" id="student" name="student">
+                                <label for="student">Étudiant</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="representative" name="representative">
+                                <label for="representative">Délégué</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="pilot" name="pilot">
+                                <label for="pilot">Pilote</label>
+                            </div>
                         
-                        <h3>Localité</h3>
-                        <div>
-                            <input type="checkbox" id="paris" name="paris">
-                            <label for="paris">Paris</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" id="bordeaux" name="bordeaux">
-                            <label for="bordeaux">Bordeaux</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" id="merignac" name="merignac">
-                            <label for="merignac">Mérignac</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" id="lyon" name="lyon">
-                            <label for="lyon">Lyon</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" id="olympe" name="olympe">
-                            <label for="olympe">Mont Olympe</label> 
-                        </div>
+                            <h3>Localité</h3>
+                            <div>
+                                <input type="checkbox" id="paris" name="paris">
+                                <label for="paris">Paris</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="bordeaux" name="bordeaux">
+                                <label for="bordeaux">Bordeaux</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="merignac" name="merignac">
+                                <label for="merignac">Mérignac</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="lyon" name="lyon">
+                                <label for="lyon">Lyon</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="olympe" name="olympe">
+                                <label for="olympe">Mont Olympe</label> 
+                            </div>
                         
-                        <h3>Promotion</h3>
-                        <div>
-                            <input type="checkbox" id="info" name="info">
-                            <label for="info">A2 Informatique</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" id="generaliste" name="generaliste">
-                            <label for="generaliste">A2 Généraliste</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" id="btp" name="btp">
-                            <label for="btp">A2 BTP</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" id="s3e" name="s3e">
-                            <label for="s3e">A2 S3E</label>
-                        </div>
-                </div>
-            </div>-->
+                            <h3>Promotion</h3>
+                            <div>
+                                <input type="checkbox" id="info" name="info">
+                                <label for="info">A2 Informatique</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="generaliste" name="generaliste">
+                                <label for="generaliste">A2 Généraliste</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="btp" name="btp">
+                                <label for="btp">A2 BTP</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="s3e" name="s3e">
+                                <label for="s3e">A2 S3E</label>
+                            </div>
+                </div>-->
+            </div>
             
             <div class="profil_card">
             
