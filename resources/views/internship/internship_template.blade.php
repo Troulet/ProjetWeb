ad2f677ec61572ff919fc5122e311d297ad8379e
Silvio <Silvio@DESKTOP-GMKS6CP>
2021-04-04 20:35:21 +0200

    Internship correct

--- resources/views/internship/internship_template.blade.php
+++ resources/views/internship/internship_template.blade.php
@@ -6,30 +6,36 @@
     <div class="carousel-item active">
             <div class="row">
                     @php
-                    $i=1
+                        $i=1
                     @endphp
+
                     @if($dataOffer !== null)
-                    @foreach($dataOffer as $Offer)
-                        <div id="banner-{{$i}}" class="col-sm">
-                        <img class="d-block w-100 desk-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="{{$i}} slide">
-                        <img class="w-100  mobile-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="{{$i}} slide">
-                        <h5 class="card-title">Stage chez {{$Offer['Enterprise_Name']}}</h5>
-                        <p class="card-text">
-                            Desccription : {{$Offer['Description']}} </br>
-                            Compétences recherchées : {{$Offer['Skills_Researched']}} </br>
-                            Promotion ciblée : {{$Offer['Promotion_Researched']}} </br>
-                            Nombre de places : {{$Offer['Number_Of_Places']}} </br>
-                            Localisation : {{$Offer['Localisation_Name']}} </br>
-                        </p>
-                        {!! Form::open(['url' => '/Offer_Profil', 'method' => 'post']) !!}
-                            <input name='id' type="hidden" value="{{$Offer['id']}}">
-                            <button type="submit" class="btn">Aller voir</button>
-                        {!! Form::close() !!}
-                        </div>
-                    @php
-                        $i++
-                    @endphp
-                    @endforeach
+
+                        @foreach($dataOffer as $Offer)
+                            @if($Offer['deleted_at'] != null && $user_type != 0)
+                            <div id="banner-{{$i}}" class="col-sm">
+                            <img class="d-block w-100 desk-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="{{$i}} slide">
+                            <img class="w-100  mobile-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="{{$i}} slide">
+                            <h5 class="card-title">Stage chez {{$Offer['Enterprise_Name']}}</h5>
+                            <p class="card-text">
+                                Desccription : {{$Offer['Description']}} </br>
+                                Compétences recherchées : {{$Offer['Skills_Researched']}} </br>
+                                Promotion ciblée : {{$Offer['Promotion_Researched']}} </br>
+                                Nombre de places : {{$Offer['Number_Of_Places']}} </br>
+                                Localisation : {{$Offer['Localisation_Name']}} </br>
+                            </p>
+                            {!! Form::open(['url' => '/Offer_Profil', 'method' => 'post']) !!}
+                                <input name='id' type="hidden" value="{{$Offer['id']}}">
+                                <button type="submit" class="btn">Aller voir</button>
+                            {!! Form::close() !!}
+                            </div>       
+                            @php
+                                $i++
+                            @endphp
+
+                            @endif
+                        @endforeach
+
                     @endif
 
                     @php
@@ -37,6 +43,7 @@
                     @endphp
                     @if($dataEnterprise !== null)
                     @foreach($dataEnterprise as $Enterprise)
+                    @if($Offer['deleted_at'] != null && $user_type != 0)
                         <div id="banner-{{$i}}" class="col-sm">
                         <img class="d-block w-100 desk-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="{{$i}} slide">
                         <img class="w-100  mobile-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="{{$i}} slide">
@@ -54,6 +61,8 @@
                     @php
                         $i++
                     @endphp
+                    @endif
+
                     @endforeach
                     @endif
 
--- resources/views/welcome/welcome_template.blade.php
+++ resources/views/welcome/welcome_template.blade.php
@@ -2,41 +2,71 @@
 
 @section('carousel_enterprise')
 
-    <div class="card-body">
-            <div id="banner-{{$i}}" class="col-sm">
-                @foreach($dataOffer as $Offer)
-                @php
-                    $i=1
-                @endphp
-            
-                    <img class="d-block w-100 desk-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="{{$i}} slide">
-                    <img class="w-100  mobile-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="{{$i}}  slide">
-                    <h5 class="card-title">Stage chez {{$Offer['Enterprise_Name']}}</h5>
-                    <p class="card-text">
-                        {{$Offer['Description']}} </br>
-                        Compétences recherchées : {{$Offer['Skills_Researched']}} </br>
-                        Promotion ciblée : {{$Offer['Promotion_Researched']}} </br>
-                        Nombre de places : {{$Offer['Number_Of_Places']}} </br>
-                        Localisation : {{$Offer['Localisation_Name']}} </br>
-                    </p>
-                    <a href="#" class="btn btn-primary">Aller voir</a>
-                @php
-                    $i++
-                @endphp
-                @endforeach
+<div class="card-body">
+    <div class="carousel-item active">
+            <div class="row">
+                    @php
+                        $i=1
+                    @endphp
 
-                @foreach($dataEnterprise as $Enterprise)
+                    @if($dataOffer !== null)
 
-                    <h5 class="card-title">Entreprise {{$Enterprise['Enterprise_Name']}}</h5>
-                    <p class="card-text">
-                    Travaille dans le secteur : {{$Enterprise['Activity_Sector']}} </br>
-                    A déjà pris {{$Enterprise['Cesi_Student_Taken']}} élèves. </br>
-                    Nombre de places : {{$Offer['Number_Of_Places']}} </br>
-                    Se situe à {{$Enterprise['Localisation_Name']}} ! </br>
-                    </p>
-                    <a href="#" class="btn btn-primary">Aller voir</a>
+                        @foreach($dataOffer as $Offer)
+                            @if($Offer['deleted_at'] != null && $user_type != 0)
+                            <div id="banner-{{$i}}" class="col-sm">
+                            <img class="d-block w-100 desk-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="{{$i}} slide">
+                            <img class="w-100  mobile-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="{{$i}} slide">
+                            <h5 class="card-title">Stage chez {{$Offer['Enterprise_Name']}}</h5>
+                            <p class="card-text">
+                                Desccription : {{$Offer['Description']}} </br>
+                                Compétences recherchées : {{$Offer['Skills_Researched']}} </br>
+                                Promotion ciblée : {{$Offer['Promotion_Researched']}} </br>
+                                Nombre de places : {{$Offer['Number_Of_Places']}} </br>
+                                Localisation : {{$Offer['Localisation_Name']}} </br>
+                            </p>
+                            {!! Form::open(['url' => '/Offer_Profil', 'method' => 'post']) !!}
+                                <input name='id' type="hidden" value="{{$Offer['id']}}">
+                                <button type="submit" class="btn">Aller voir</button>
+                            {!! Form::close() !!}
+                            </div>       
+                            @php
+                                $i++
+                            @endphp
 
-                @endforeach
+                            @endif
+                        @endforeach
+
+                    @endif
+
+                    @php
+                        $i=1
+                    @endphp
+                    @if($dataEnterprise !== null)
+                    @foreach($dataEnterprise as $Enterprise)
+                    @if($Offer['deleted_at'] != null && $user_type != 0)
+                        <div id="banner-{{$i}}" class="col-sm">
+                        <img class="d-block w-100 desk-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="{{$i}} slide">
+                        <img class="w-100  mobile-img" src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" alt="{{$i}} slide">
+                        <h5 class="card-title">Entreprise {{$Enterprise['Enterprise_Name']}}</h5>
+                        <p class="card-text">
+                            Travaille dans le secteur : {{$Enterprise['Activity_Sector']}} </br>
+                            A déjà pris {{$Enterprise['Cesi_Student_Taken']}} élèves. </br>
+                            Se situe à {{$Enterprise['Localisation_Name']}} ! </br>
+                        </p>
+                        {!! Form::open(['url' => '/Enterprise_Profil', 'method' => 'post']) !!}
+                            <input name='id' type="hidden" value="{{$Enterprise['id']}}">
+                            <button type="submit" class="btn">Aller voir</button>
+                        {!! Form::close() !!}
+                        </div>
+                    @php
+                        $i++
+                    @endphp
+                    @endif
+
+                    @endforeach
+                    @endif
+
             </div>
     </div>
+</div>
 @stop
