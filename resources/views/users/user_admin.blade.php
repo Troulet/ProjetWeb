539b9ee5eca06cc30ad677a056886bf8fc38fc64
Clémentine COUTANT <clementine.coutant@viacesi.fr>
2021-04-04 23:00:49 +0200

    op

--- resources/views/users/user_admin.blade.php
+++ resources/views/users/user_admin.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <link rel= "stylesheet" href="../resources/views/header_footer.css"/>
+        <link rel= "stylesheet" href="../resources/views/header_footer.css">
         <link rel= "stylesheet" href="../resources/views/user_admin.css">
         <title>LES P'TITES ANNONCES</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
