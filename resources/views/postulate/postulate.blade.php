56cc507c028ad016ee1d85c1f11924ee5feaf547
Inès Casenave <ines.ath.casenave@gmail.com>
2021-04-05 19:07:54 +0200

    jad qui fait oh

--- resources/views/postulate/postulate.blade.php
+++ resources/views/postulate/postulate.blade.php
@@ -12,58 +12,13 @@
     </head>
         
     <body>
-            @include('header')
-        <div id="id_candidature" class="candidature">
-            <h2>Statistiques</h2>
-            <label for="post_nbre"> Candidatures déposées:</label><br>
-            <input type="number" name="post_nbre" id="post_nbre" required><br><br>
+        @include('header')
 
-            <label for="date"> Date de la dernière candidature postée:</label><br>
-            <input type="Date" name="date" id="date" required><br><br>
-            
-            <button class="buton" type="button">CANDIDATURE AVEC RÉPONSE</button>
-            <button class="buton" type="button">CANDIDATURE SANS RÉPONSE</button>
-            <button class="buton" type="button">TOUT</button>
-        </div>
-        <table class="table">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Entreprise</th>
-                  <th scope="col">Intitulé du poste</th>
-                  <th scope="col">Curriculum vitae</th>       <!--Réponse = oui/non uniquement-->
-                  <th scope="col">Lettre de motivation</th>   <!--Réponse = oui/non uniquement-->
-                  <th scope="col">Réponse</th>                <!--Réponse = oui/non uniquement-->
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-              </tbody>
-            </table>
-        <div class="wishlist">
+        @yield('candidate_status')
+
+        @yield('tab_candidate')
+
+        <!--<div class="wishlist">
         <div class="list-group">
             <h3>Wish-List</h3>
             <a href="#" class="list-group-item list-group-item-action">Choix 1</a>
@@ -71,7 +26,8 @@
             <a href="#" class="list-group-item list-group-item-action">Choix 3</a>
             <a href="#" class="list-group-item list-group-item-action">Choix 4</a>
         </div>
-    </div>
+        </div>-->
+
         @include('footer')
     </body>
 
---
+++ resources/views/postulate/postulate_template.blade.php
@@ -0,0 +1,79 @@
+@extends(postulate.postulate)
+
+@section('candidate_status')
+
+    <div id="id_candidature" class="candidature">
+    <h2>Statistiques</h2>
+    <label for="post_nbre"> Candidatures déposées:</label><br>
+    <input type="number" name="post_nbre" id="post_nbre" required><br><br>
+
+    <label for="Candidature_Date">Date de la dernière candidature postée:</label><br>
+    <input type="Date" name="Candidature_Date" id="Candidature_Date" required><br><br>
+            
+    <button class="button" type="button">CANDIDATURE AVEC RÉPONSE</button>
+    <button class="button" type="button">CANDIDATURE SANS RÉPONSE</button>
+    <button class="button" type="button">TOUT</button>
+    </div>
+
+@stop
+
+@section('tab_candidate')
+
+
+<table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Entreprise</th>
+                  <th scope="col">Intitulé du poste</th>
+                  <th scope="col">Curriculum vitae</th>       <!--Réponse = oui/non uniquement-->
+                  <th scope="col">Lettre de motivation</th>   <!--Réponse = oui/non uniquement-->
+                  <th scope="col">Réponse</th>                <!--Réponse = oui/non uniquement-->
+                </tr>
+              </thead>
+              <tbody>
+              @foreach($dataOffer as $Offer)
+                <tr>
+                  <th scope="row">1</th>
+                  <td>{{$Offer['Enterprise_Name']}}</td>
+                  <td><a href="/Offer/{{$Offer['id']}}" >Offre</a> </td> <!--form avec Internship_id-->
+                  <td>{{echo $Offer['Curriculum_Vitae']}}</td>
+                  <td>{{echo $Offer['Motivation_Letter']}}</td>
+
+                  @if($dataOffer['Response_State'] == 0)
+                  <td><select name="Response_State">
+	                    <option selected="yes">Non</option>
+	                    <option>Oui</option>
+                      </select></td>
+                  @elseif ($dataOffer['Response_State'] == 1)
+                  <td><select name="Response_State">
+	                    <option selected="yes">Oui</option>
+	                    <option>Non</option>
+                      </select></td>
+                  @endif
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+               @endforeach
+              </tbody>
+        </table>
+
+
+<a href="../index.html"> ... </a>
+
+
+@stop
