372c10cf4ae4c23ea413698532df699ba3117033
Inès Casenave <ines.ath.casenave@gmail.com>
2021-04-05 20:14:52 +0200

    rr

---
+++ resources/views/postulate/show_pdf.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel= "stylesheet" href="../resources/views/postulate/postulate.css"/>
+        <link rel= "stylesheet" href="../resources/views/header_footer.css"/>
+        <title>LES P'TITES ANNONCES</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="https://fonts.googleapis.com/css2?family=Happy+Monkey&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head>
+        
+    <body>
+        @include('header')
+
+       
+
+        @include('footer')
+    </body>
+
+    
+</html>
