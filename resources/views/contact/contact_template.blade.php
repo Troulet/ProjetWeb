76830ae9bc075979d55ac18fe2b5be5ff041c772
Inès Casenave <ines.ath.casenave@gmail.com>
2021-04-05 12:16:02 +0200

    modif contact template

--- resources/views/contact/contact_template.blade.php
+++ resources/views/contact/contact_template.blade.php
@@ -1,48 +1,69 @@
 @extends('contact.contact_admin')
+@extends('contact.contact_pilot')
+@extends('contact.contact_student')
 
 @section('data_student')
-    @foreach ($dataStudent as $user)
-    <div class="card-body">
-    <p>
-        Elève :
-        Email : {{$user['email']}}
-        Prénom : {{$user['First_Name']}}
-        Nom : {{$user['Last_name']}}
-        Promotion : {{$user['Promotion']}}
+    @if($dataStudent !== null)
+        @foreach ($dataStudent as $user)
+        <div class="card-body">
+            <p>
+                Elève :
+                Email : {{$user['email']}}
+                Prénom : {{$user['First_Name']}}
+                Nom : {{$user['Last_name']}}
+                Promotion : {{$user['Promotion']}}
 
-        @if($user['Representative']==true)
-            Délégué : oui
-        @else
-            Délégué : non
-        @endif
-    </p>
-    </div>
-    @endforeach
+                @if($user['Representative']==true)
+                    Délégué : oui
+                @else
+                    Délégué : non
+                @endif
+            </p>
+                {!! Form::open(['url' => '/Contact', 'method' => 'get']) !!}
+                        <input name='id' type="hidden" value="{{$user['Users_id']}}">
+                        <button type="submit" class="btn">Contacter</button>
+                {!! Form::close() !!}
+        </div>
+        @endforeach
+    @endif
     
 @stop
 
 @section('data_pilot')
-    @foreach ($dataPilot as $user)
-    <div class="card-body">
-        <p>
-            Information Pilote :
-            Email : {{$user['email']}}
-            Prénom : {{$user['First_Name']}}
-            Nom : {{$user['Last_name']}}
-            Promotion : {{$user['Promotion']}}
-        </p>
-    </div>
-    @endforeach
+    @if($dataPilot !== null)
+        @foreach ($dataPilot as $user)
+        <div class="card-body">
+            <p>
+                Information Pilote :
+                Email : {{$user['email']}}
+                Prénom : {{$user['First_Name']}}
+                Nom : {{$user['Last_name']}}
+                Promotion : {{$user['Promotion']}}
+            </p>
+            {!! Form::open(['url' => '/Contact', 'method' => 'get']) !!}
+                    <input name='id' type="hidden" value="{{$user['Users_id']}}">
+                    <button type="submit" class="btn">Contacter</button>
+            {!! Form::close() !!}
+        </div>
+        @endforeach
+    @endif
 @stop
 
 @section('data_admin')
-    @foreach ($dataAdmin as $user)
-    <div class="card-body">
-        <p>
-            Information Admin :
-            Email : {{$user['email']}}
-            Prénom : {{$user['First_Name']}}
-            Nom : {{$user['Last_name']}}
-        </p>
-    </div>
+    @if($dataAdmin !== null)
+        @foreach ($dataAdmin as $user)
+        <div class="card-body">
+            <p>
+                Information Admin :
+                Email : {{$user['email']}}
+                Prénom : {{$user['First_Name']}}
+                Nom : {{$user['Last_name']}}
+            </p>
+            {!! Form::open(['url' => '/Contact', 'method' => 'get']) !!}
+                    <input name='id' type="hidden" value="{{$user['Users_id']}}">
+                    <button type="submit" class="btn">Contacter</button>
+            {!! Form::close() !!}
+        </div>
+        @endforeach
+    @endif
 @stop
