3ee6fae9098b8b4e1eb438148a073ecf39a677b4
Inès Casenave <ines.ath.casenave@gmail.com>
2021-04-06 00:23:58 +0200

    yh

--- resources/views/users/User_profile.blade.php
+++ resources/views/users/User_profile.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charsert="utf-8" />
-        <link rel= "stylesheet" href="../resources/views/internship/internship_pilot.css"/>
+        <link rel= "stylesheet" href="../resources/views/users/user_admin.css"/>
         <link rel= "stylesheet" href="../resources/views/header_footer.css"/>
         <title>LES P'TITES ANNONCES</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
